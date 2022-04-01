5d8acebf91dfc832aaddd1299c1733eb708be1a6
Jold10x <van_tu.nguyen@master-stic.fr>
2022-04-01 13:34:17 +0000

    TP4 Joseph NGUYEN

--- src/models/iptables/nat.js
+++ src/models/iptables/nat.js
@@ -17,7 +17,7 @@ class Nat {
 
   async getNatList() {
     try {
-      this.natList = await connexion.query("SELECT idNat, nameNat, port, type FROM nat_rules");
+      this.natList = await connexion.query("SELECT idNat, nameNat, port, ipAddress,  type FROM nat_rules");
 
       return this.natList;
     }
